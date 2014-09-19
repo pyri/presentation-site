4226c265c0760e57e14d8a43dcc5cdbeda3f0a2c
pyri <pyrikv@inbox.ru>
2014-09-19 19:18:04 +0400

    Changed styles. Created structure of main html-page, header, main menu.

--- view/pages/mainpage_view.php
+++ view/pages/mainpage_view.php
@@ -1,11 +1,12 @@
 ﻿<?php $articles = Model::readMainPage(); ?>
 <div class="content border">
-    <? foreach ($articles as $article): ?>
+    <img src="../img/slider.png" alt="Картинка для слайдера"/>
+    <? /*foreach ($articles as $article): ?>
         <h2><?php echo $article->title1; ?></h2>
         <h3><?php echo $article->title2; ?></h3>
         <p>
             <?php echo $article->article; ?>
         </p>
-    <? endforeach; ?>
+    <? endforeach; */?>
 
 </div>
--- view/template.php
+++ view/template.php
@@ -3,30 +3,67 @@
 <head>
     <meta charset="UTF-8">
     <title>Кипайкина К. - тестовое задание</title>
+    <link rel="stylesheet" href="../css/normalize.css">
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-<div class="wrapper">
-    <header class="border">
-        <ul>
-            <li class="headerli-first"><a href="/">Главная</a></li>
-            <li class="headerli-second"><a href="/?page=about_me">Обо мне</a></li>
-            <li class="headerli-third"><a href="/?page=comment">Комментарии</a></li>
-        </ul>
-    </header>
+    <div id="wrapper">
+        <header>
+            <div class="logo">
+                <img src="../img/logo.png" alt="Логотип"/>
+            </div>
 
-    <div class="main">
-        <aside class="sidebar-right border">
+            <div class="dotted_line">
+                <img src="../img/dotted_line.png" alt="Линия разделения областей"/>
+            </div>
+            <div class="slogan">
+                <p>Мы работаем для вашего результата!</p>
+            </div>
+
+            <div class="auth">
+                <p>Авторизация / Регистрация</p>
+            </div>
+        </header>
+
+        <div id="mainmenu">
             <ul>
-                <li class="partition-dashed"><a href="/">Главная</a></li>
-                <li class="partition-dashed"><a href="/?page=about_me">Обо мне</a></li>
-                <li><a href="/?page=comment">Комментарии</a></li>
+                <li class="active_li"><a href="#" class="active_a">Главная</a></li>
+                <li><a href="#">Обо мне</a></li>
+                <li><a href="#">Комментарии</a></li>
             </ul>
-        </aside>
+        </div>
 
-        <?php Route::renderContent(); ?>
+        <div id="main">
+            <aside class="sidebar-right border">
+                <img src="../img/frame.png" alt="Фоторамка для таблицы"/>
+                <!--<ul>
+                    <li class="partition-dashed"><a href="/">Главная</a></li>
+                    <li class="partition-dashed"><a href="/?page=about_me">Обо мне</a></li>
+                    <li><a href="/?page=comment">Комментарии</a></li>
+                </ul>-->
+            </aside>
+
+            <?php Route::renderContent(); ?>
+        </div>
+
+        <footer>
+            это три  ul :)
+
+            <ul>
+                <li>О компании</li>
+                <li>На сегодняшний день...</li>
+            </ul>
+            <ul>
+                <li>Новости</li>
+                <li>На сегодняшний день...</li>
+            </ul>
+            <ul>
+                <li>Проценты на сегодня</li>
+                <li>На сегодняшний день...</li>
+            </ul>
+            <input type="button" name="next" value="Далее"/>
+        </footer>
     </div>
-</div>
 <script src="http://code.jquery.com/jquery-2.1.1.js"></script>
 <script type="text/javascript" src="../js/comment.js"></script>
 </body>
